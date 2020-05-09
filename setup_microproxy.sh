16b1088048e7cd6f055c64933f0acd9718fe8bed
sysadmin001 <59505521+sysadmin001@users.noreply.github.com>
2020-05-09 19:50:26 +0600

    Update setup_microproxy.sh

--- setup_microproxy.sh
+++ setup_microproxy.sh
@@ -3,15 +3,15 @@ set -e
 
 mkdir -p /tmp/proxy && cd $_
 wget https://github.com/sysadmin001/gcp/raw/master/proxy.tar.gz
-tar xzf proxy.tar.gz -C /tmp/proxy/
+tar xzf proxy.tar.gz -C ./
 
 # https://github.com/thekvs/microproxy
 sudo useradd microproxy -s /sbin/nologin
 sudo mkdir -p /opt/microproxy
 sudo chown microproxy:microproxy /opt/microproxy
-sudo cp microproxy /opt/microproxy/
+sudo cp proxy/microproxy* /opt/microproxy/
 sudo chmod 755 /opt/microproxy/microproxy
-sudo cp microproxy.service /lib/systemd/system/microproxy.service
+sudo cp proxy/microproxy.service /lib/systemd/system/microproxy.service
 sudo chmod 755 /lib/systemd/system/microproxy.service
 # sudo systemctl enable microproxy
 # sudo systemctl start microproxy
